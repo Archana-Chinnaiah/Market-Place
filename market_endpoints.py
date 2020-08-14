8c5feb3ad78d4bf7237d0bc25be89b7a1081913f
Archana-Chinnaiah <66065120+Archana-Chinnaiah@users.noreply.github.com>
2020-08-14 20:30:00 +0530

    Update market_endpoints.py

--- market_endpoints.py
+++ market_endpoints.py
@@ -49,6 +49,19 @@ def item_list(category_id):
         items_list.append(obj)
     return jsonify(items_list)
 
+@app.route("/cart/<customer_id>", methods=["GET"])
+def item_list(customer_id):
+    items_in_cart = session.query(CustomerCart).filter_by(customer_id=customer_id).all()
+    cart_list = []
+    for item in items_in_cart:
+        obj = {
+            'item id': item.item_id,
+            'quantity': item.quantity,
+            'Total_price': int(item.total_price)
+        }
+        cart_list.append(obj)
+    return jsonify(cart_list)
+
 @app.route("/cart", methods=["POST"])
 def add_to_cart():
     customer_id = request.form['customer_id']
