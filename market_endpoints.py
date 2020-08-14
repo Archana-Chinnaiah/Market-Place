df593f1ea2806834e40b9540512ebb54708aac95
Archana C <archanachinnaiah@gmail.com>
2020-08-14 19:12:29 +0530

    market place platform database ,endpoints and classes

--- market_endpoints.py
+++ market_endpoints.py
@@ -1,39 +1,95 @@
 from flask import Flask, jsonify, request
-from marketplace import Category, Item, Customer, CustomerCart
+from marketplace import Category, Item,Customer, CustomerCart
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 engine = create_engine("postgresql://postgres:archu@localhost:1521/market_place")
 Session = sessionmaker(bind=engine)
 session = Session()
+app = Flask(__name__)
+app.config["DEBUG"] = True
 
-app=Flask(__name__)
-app.config["DEBUG"]=True
+@app.route("/login", methods=["GET","POST"])
+def login():
+    if request.method == "POST":
+        username = request.form['username']
+        password = request.form['password']
+        user = session.query(Customer).filter_by(username=username,password=password).first()
+        if user:
+            return "Logged in Successfully"
+        else:
+            return "Invalid Credentials"
 
-
-@app.route("/categories", methods = ["GET"])
+@app.route("/categories", methods=["GET"])
 def list_categories():
-    categories=[]
-    category =session.query(Category).all()
+    categories = []
+    category = session.query(Category).all()
+
     for category in category:
         obj = {
             'id': category.category_id,
             'name': category.category_name,
-            'description':category.description
+            'description': category.description
         }
         categories.append(obj)
     return jsonify(categories)
 
+@app.route("/categories/<category_id>/items", methods=["GET"])
+def item_list(category_id):
+    items = session.query(Item).filter_by(category_id=category_id).all()
+    items_list = []
+    for item in items:
+        obj = {
+            'id': item.item_id,
+            'name': item.item_name,
+            'seller name': item.seller_name,
+            'available quantity': item.available_quantity,
+            'description': item.description
+        }
+        items_list.append(obj)
+    return jsonify(items_list)
 
-@app.route("/cart", methods = ["POST"])
+@app.route("/cart", methods=["POST"])
 def add_to_cart():
     customer_id = request.form['customer_id']
     item_id = request.form['item_id']
     quantity = request.form['quantity']
-    result=CustomerCart(customer_id =customer_id,item_id=item_id,quantity=quantity)
-    session.add(result)
+    exists = session.query(CustomerCart).filter_by(customer_id=customer_id,item_id=item_id).first()
+    if exists:
+        return "This item has been already in your cart"
+    else:
+        avail_quantity = session.query(Item).filter_by(item_id=item_id).first()
+        if int(avail_quantity.available_quantity) >= int(quantity):
+            total = int(avail_quantity.price) * int(quantity)
+            result = CustomerCart(customer_id=customer_id, item_id=item_id, quantity=quantity, total_price=total)
+            session.add(result)
+            session.commit()
+            return "Successfully Added to cart"
+        else:
+            return "Out of stock"
+
+@app.route("/cart/<cart_id>", methods=["PUT"])
+def update_quantity(cart_id):
+    quantity = request.form['quantity']
+    cart = session.query(CustomerCart).filter_by(id=cart_id).first()
+    item = cart.item_id
+    need = session.query(Item).filter_by(item_id=item).first()
+    cart.quantity = quantity
+    cart.total_price = int(quantity) * int(need.price)
+    session.add(cart)
     session.commit()
-    return "Successfully Added to cart"
+    return "Updated Successfully"
+
+@app.route("/cart/<cart_id>", methods=["DELETE"])
+def delete_items(cart_id):
+    item = session.query(CustomerCart).filter_by(id=cart_id).one()
+    session.delete(item)
+    session.commit()
+    return "deleted Successfully"
+
+@app.route("/logout",methods=["GET"])
+def logout():
+    return "you have logged out successfully"
 
 
 if __name__ == "__main__":
--- marketplace.py
+++ marketplace.py
@@ -6,28 +6,27 @@ Base = declarative_base()
 class Category(Base):
     __tablename__ = 'category'
     category_id = Column(Integer, primary_key=True)
-    category_name = Column(String,unique=True,nullable=False)
-    description = Column(String,nullable=False)
+    category_name = Column(String, unique=True, nullable=False)
+    description = Column(String, nullable=False)
 
 
 class Item(Base):
     __tablename__ = 'item'
     item_id = Column(Integer, primary_key=True)
-    item_name = Column(String,nullable=False)
-    price = Column(Integer,nullable=False)
-    seller_name = Column(String,nullable=False)
-    availabale_quantity = Column(Integer)
+    item_name = Column(String, nullable=False)
+    price = Column(Integer, nullable=False)
+    seller_name = Column(String, nullable=False)
+    description = Column(String, nullable=False)
+    available_quantity = Column(Integer)
     category_id = Column(Integer, ForeignKey('Category.category_id'))
 
 class Customer(Base):
     __tablename__ = 'customer'
     customer_id = Column(Integer, primary_key=True)
-    username  = Column(String,unique=True,nullable=False)
-    password = Column(String,unique=True,nullable=False)
-    address = Column(String,unique=True,nullable=False)
-    mobile_no = Column(Integer,nullable=False)
-
-
+    username = Column(String, unique=True, nullable=False)
+    password = Column(String, unique=True, nullable=False)
+    address = Column(String, unique=True, nullable=False)
+    mobile_no = Column(Integer, nullable=False)
 
 class CustomerCart(Base):
     __tablename__ = 'customer_cart'
